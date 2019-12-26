3defca45b50beb610da6572178824727152ffdd6
santosh <aadarshamag@gmail.com>
2019-12-26 12:40:47 +0545

    homepage designed for first time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },  {
+    path: 'homepage',
+    loadChildren: () => import('./homepage/homepage.module').then( m => m.HomepagePageModule)
   }
+
 ];
 
 @NgModule({
