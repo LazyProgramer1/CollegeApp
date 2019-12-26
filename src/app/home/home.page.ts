5fb986a8e18e346033e674cef5f27c4b65b75929
santosh <aadarshamag@gmail.com>
2019-12-26 14:57:17 +0545

    colleges

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  colleges={"urlToImage":"https://patancollege.edu.np/wp-content/uploads/2018/06/DVS180516-1358.jpg"
+};
 
   constructor() {}
 
